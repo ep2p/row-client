292cacd7e74fb3824c9367f6e4d30b8e35eee973
sepehr <sepehrgh01@gmail.com>
2020-07-22 16:06:35 +0430

    Fixed packaging

--- src/main/java/labs/psychogen/row/client/callback/SubscriptionCallback.java
+++ src/main/java/labs/psychogen/row/client/callback/SubscriptionCallback.java
@@ -1,4 +1,5 @@
 package labs.psychogen.row.client.callback;
+import labs.psychogen.row.client.Subscription;
 
 public interface SubscriptionCallback<E> extends ResponseCallback<E> {
     Subscription getSubscription();
