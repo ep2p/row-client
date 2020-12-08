26c090f987f3b0d2bef4dd4922b4597deae20115
sepehr-gh <sepehrgh01@gmail.com>
2020-12-08 11:50:07 +0330

    Once switched to fallback, it will be used for next requests

--- src/main/java/lab/idioglossia/row/client/HttpFallbackRowClientDecorator.java
+++ src/main/java/lab/idioglossia/row/client/HttpFallbackRowClientDecorator.java
@@ -1,12 +1,14 @@
 package lab.idioglossia.row.client;
 
 import lab.idioglossia.row.client.callback.ResponseCallback;
+import lab.idioglossia.row.client.callback.SubscriptionListener;
 import lab.idioglossia.row.client.model.RowRequest;
 
 import java.io.IOException;
 
 public class HttpFallbackRowClientDecorator extends RowClientDecorator {
     private final RowHttpClient rowHttpClient;
+    private volatile boolean switched = false;
 
     public HttpFallbackRowClientDecorator(RowClient rowClient, RowHttpClient rowHttpClient) {
         super(rowClient);
@@ -15,15 +17,31 @@ public class HttpFallbackRowClientDecorator extends RowClientDecorator {
 
     @Override
     public void sendRequest(RowRequest<?, ?> rowRequest, ResponseCallback<?> callback) {
-        try {
-            super.sendRequest(rowRequest, callback);
-        }catch (IOException | IllegalStateException e){
-            rowHttpClient.sendRequest(rowRequest, callback);
+        if(!switched){
+            try {
+                super.sendRequest(rowRequest, callback);
+                return;
+            }catch (IOException | IllegalStateException ignored){
+                switched = true;
+            }
         }
+        rowHttpClient.sendRequest(rowRequest, callback);
+    }
+
+    @Override
+    public void subscribe(RowRequest<?, ?> rowRequest, ResponseCallback<?> callback, SubscriptionListener<?> subscriptionListener) throws IOException {
+        if(switched){
+            throw new IOException("Cant send subscribe on HTTP Fallback client");
+        }
+        super.subscribe(rowRequest, callback, subscriptionListener);
     }
 
     @Override
     public void open() {
-        super.open();
+        try {
+            super.open();
+        }catch (Exception e){
+            switched = true;
+        }
     }
 }
